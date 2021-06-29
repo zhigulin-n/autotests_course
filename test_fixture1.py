4bd0e2856b7128f30a05841a806d97f9604d4ecb
n.zhigulin <nikita1number@gmail.com>
2021-06-29 18:00:36 +0300

    upd

--- test_fixture1.py
+++ test_fixture1.py
@@ -7,19 +7,21 @@ class TestMainPage1():
 
     @classmethod
     def setup_class(self):
-        print("\nstart browser for test suite..")
+        print("\nstart browser for test suite 1 ..")
         self.browser = webdriver.Chrome()
 
     @classmethod
     def teardown_class(self):
-        print("quit browser for test suite..")
+        print("quit browser for test suite 1 ..")
         self.browser.quit()
 
     def test_guest_should_see_login_link(self):
+        print('start test link 1')
         self.browser.get(link)
         self.browser.find_element_by_css_selector("#login_link")
 
     def test_guest_should_see_basket_link_on_the_main_page(self):
+        print('start test basket 1')
         self.browser.get(link)
         self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
 
@@ -27,17 +29,19 @@ class TestMainPage1():
 class TestMainPage2():
 
     def setup_method(self):
-        print("start browser for test..")
+        print("start browser for test 2..")
         self.browser = webdriver.Chrome()
 
     def teardown_method(self):
-        print("quit browser for test..")
+        print("quit browser for test 2..")
         self.browser.quit()
 
     def test_guest_should_see_login_link(self):
+        print('start test link 2')
         self.browser.get(link)
         self.browser.find_element_by_css_selector("#login_link")
 
     def test_guest_should_see_basket_link_on_the_main_page(self):
+        print('start test basket 2')
         self.browser.get(link)
         self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
